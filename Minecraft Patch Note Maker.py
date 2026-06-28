d2a86c65c20260310db3f5f50230550e35d985b4
5UP3R TH30 B4G <68150571+5UP3RTH30B4G@users.noreply.github.com>
2026-06-28 17:10:00 +0200

    added a sick Ascii art at boot along side a version variable

--- Minecraft Patch Note Maker.py
+++ Minecraft Patch Note Maker.py
@@ -1,9 +1,22 @@
-# Minecraft Patch Note Maker 1.1
+# Minecraft Patch Note Maker 1.2
 
 import json
 import zipfile
 import click
 from tkinter import Tk, filedialog
+
+app_version = "1.2"
+
+print(fr"""
+  __  __ _                            __ _     _____      _       _       _   _       _         __  __       _             
+ |  \/  (_)                          / _| |   |  __ \    | |     | |     | \ | |     | |       |  \/  |     | |            
+ | \  / |_ _ __   ___  ___ _ __ __ _| |_| |_  | |__) |_ _| |_ ___| |__   |  \| | ___ | |_ ___  | \  / | __ _| | _____ _ __ 
+ | |\/| | | '_ \ / _ \/ __| '__/ _` |  _| __| |  ___/ _` | __/ __| '_ \  | . ` |/ _ \| __/ _ \ | |\/| |/ _` | |/ / _ \ '__|
+ | |  | | | | | |  __/ (__| | | (_| | | | |_  | |  | (_| | || (__| | | | | |\  | (_) | ||  __/ | |  | | (_| |   <  __/ |   
+ |_|  |_|_|_| |_|\___|\___|_|  \__,_|_|  \__| |_|   \__,_|\__\___|_| |_| |_| \_|\___/ \__\___| |_|  |_|\__,_|_|\_\___|_|   {app_version}
+
+    Being a lazy dev has never being this easy!
+      """)
 
 def parse_version(version):
     try:
